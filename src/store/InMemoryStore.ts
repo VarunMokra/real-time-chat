ad819b481f67f670bb9638bbc8131ec618fc4ce6
VarunMokra <varunmokra1909@gmail.com>
2024-03-21 22:56:49 +0530

    added initial message handlers

--- src/store/InMemoryStore.ts
+++ src/store/InMemoryStore.ts
@@ -28,15 +28,17 @@ export class InMemoryStore implements Store {
     addChat(userId: UserId, name:string, message:string, roomId: string ){
         const room = this.store.get(roomId);
         if(!room) {
-            return;
+            return null;
         }
-        room.chats.push({
+        const chat = {
             id: (globalChatId++).toString(),
             userId,
             name,
             message,
             upvotes: []
-        })
+        };
+        room.chats.push(chat);
+        return chat;
     }
     upvote(userId: UserId, roomId: string, chatId: string){
         const room = this.store.get(roomId);
@@ -48,5 +50,6 @@ export class InMemoryStore implements Store {
         if(chat){
             chat.upvotes.push(userId);
         }
+        return chat;
     }
 }
